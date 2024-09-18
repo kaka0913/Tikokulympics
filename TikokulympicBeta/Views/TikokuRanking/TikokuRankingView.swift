d15b8e3d8c6d2694f19f58832b5d641ac7adbd6f
unmy040915 <m.yuuta0915@icloud.com>
2024-09-18 16:41:34 +0900

    usercard作成

--- TikokulympicBeta/Views/TikokuRanking/TikokuRankingView.swift
+++ TikokulympicBeta/Views/TikokuRanking/TikokuRankingView.swift
@@ -51,28 +51,26 @@ struct TextSegmentedControl: View {
     @State private var selectedIndex: Int = 0
     
     var body: some View {
-        HStack {
+        HStack(spacing:0) {
             ForEach(0..<2) { index in
                 Text(index == 0 ? "到着者" : "ランキング") // テキストに変更
-                    .font(.title)
+                    .font(index == 0 ? .system(size: 24): .system(size: 24))
+                    .fontWeight(.bold)
+                    .frame(width: 115)
                     .padding(.horizontal,40)
                     .padding(.vertical, 30)
-                    .background(self.selectedIndex == index ? Color.darkred : Color.clear)
+                    .background(self.selectedIndex == index ? Color.darkred : Color.lightgray)
                     .foregroundColor(self.selectedIndex == index ? Color.white : Color.black)
                     .clipShape(
-                                            index == 0 ?
-                                            RoundedCornerShape(corners: [.topLeft, .bottomLeft], radius: 40) :
-                                            RoundedCornerShape(corners: [.topRight, .bottomRight], radius: 40)
-                                        ) // 三項演算子で切り替え
+                        index == 0 ?
+                        RoundedCornerShape(corners: [.topLeft, .bottomLeft], radius: 40) :
+                            RoundedCornerShape(corners: [.topRight, .bottomRight], radius: 40)
+                    ) // 三項演算子で切り替え
                     .onTapGesture {
                         self.selectedIndex = index
                     }
             }
         }
-        
-        .background(Color.lightgray)
-        .cornerRadius(40)
-        .frame(width: 378, height: 60)
     }
 }
 
@@ -80,10 +78,10 @@ struct RoundedCornerShape: Shape {
     var corners: UIRectCorner
     var radius: CGFloat
     func path(in rect: CGRect) -> Path {
-            let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-            return Path(path.cgPath)
-        }
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
+}
 
 
 #Preview {
