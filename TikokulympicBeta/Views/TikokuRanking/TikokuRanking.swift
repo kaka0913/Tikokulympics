368cf97209009760f0cca33decfef4e7b27b2de6
unmy040915 <m.yuuta0915@icloud.com>
2024-09-19 00:16:01 +0900

    集合時間，場所を表示

---
+++ TikokulympicBeta/Views/TikokuRanking/TikokuRanking.swift
@@ -0,0 +1,79 @@
+
+
+import SwiftUI
+
+struct TikokuRankingView: View {
+    @State var timerHandler: Timer?
+    @State private var selectedIndex: Int = 0
+    var body: some View {
+        VStack {
+            ZStack {
+                Rectangle()
+                    .frame(height: 328)
+                    .foregroundColor(.darkred)
+                VStack {
+                    Text("残り時間")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                    Text("00:00:00")
+                        .font(.custom("", size: 100))
+                        .foregroundColor(.white)
+                   MeetingTimeInformation()
+                   LocationInformation()
+                }
+            }
+            .padding(.bottom)
+            TextSegmentedControl()
+            ScrollView {
+                RankingUserCard(rankState: .first)
+                RankingUserCard(rankState: .secound)
+                RankingUserCard(rankState: .third)
+                RankingUserCard(rankState: .normal)
+            }
+            Spacer()
+        }
+    }
+}
+
+struct TextSegmentedControl: View {
+    @State private var selectedIndex: Int = 0
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<2) { index in
+                Text(index == 0 ? "到着者" : "ランキング")  // テキストに変更
+                    .font(index == 0 ? .system(size: 24) : .system(size: 24))
+                    .fontWeight(.bold)
+                    .frame(width: 115)
+                    .padding(.horizontal, 40)
+                    .padding(.vertical, 20)
+                    .background(self.selectedIndex == index ? Color.darkred : Color.lightgray)
+                    .foregroundColor(self.selectedIndex == index ? Color.white : Color.black)
+                    .clipShape(
+                        index == 0
+                            ? RoundedCornerShape(corners: [.topLeft, .bottomLeft], radius: 40)
+                            : RoundedCornerShape(corners: [.topRight, .bottomRight], radius: 40)
+                    )
+                    .onTapGesture {
+                        self.selectedIndex = index
+                    }
+            }
+        }
+    }
+}
+
+struct RoundedCornerShape: Shape {
+    var corners: UIRectCorner
+    var radius: CGFloat
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect, byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+#Preview {
+    TikokuRankingView()
+}
