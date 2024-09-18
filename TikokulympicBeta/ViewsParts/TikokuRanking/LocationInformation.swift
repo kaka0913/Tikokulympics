3601c5fd3b8785c59f6cfa4ca9229cdef872b21c
unmy040915 <m.yuuta0915@icloud.com>
2024-09-19 00:16:01 +0900

    集合時間，場所を表示

---
+++ TikokulympicBeta/ViewsParts/TikokuRanking/LocationInformation.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct LocationInformation: View {
+    var body: some View {
+        @State var location: String = "立命館大学"
+        ZStack{
+            Rectangle()
+                .frame(width: 377, height: 30)
+                .foregroundColor(.lightgray)
+                .cornerRadius(15)
+                .padding(.bottom)
+            HStack{
+                Image("Location")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .padding(.bottom)
+                Text(location)
+                    .font(.title2)
+                    .padding(.bottom)
+            }
+        }
+    }
+}
